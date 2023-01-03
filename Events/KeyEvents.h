cd0066334735b247efc134621f2f78dda1e693f1
Hammy Cooper <thehamstarr@gmail.com>
2023-01-04 02:04:47 +0800

    key events now carry scancode

--- Events/KeyEvents.h
+++ Events/KeyEvents.h
@@ -9,43 +9,41 @@ namespace AEON
     class AEON_DLL KeyEvent : public Inherit< WindowEvent, KeyEvent >
     {
     public:
-        KeyEvent( Window* window, Key::Code& key, Key::Code& modified_key, Key::Mod& mod )
-        : Inherit( window ), m_key( key ), m_modified_key( modified_key ), m_mod( mod )
+        KeyEvent( Window* window, Key::ScanCode& code, Key::Code& key, Key::Mod& mod )
+        : Inherit( window ), m_code( code ), m_key( key ), m_mod( mod )
         {};
 
-        Key::Code       key()  const { return /*( m_mod == +Key::Mod::Shift    ) ||
-                                              ( m_mod == +Key::Mod::CapsLock ) 
-                                              ? m_modified_key
-                                              : */ m_key; }
+        Key::Mod        code() const { return m_mod; }
+        Key::Code       key()  const { return m_key; }
         Key::Mod        mod()  const { return m_mod; }
     private:
+        Key::ScanCode   m_code;
         Key::Code       m_key;
         Key::Mod        m_mod;
-        Key::Code       m_modified_key;
     };
 
     class AEON_DLL KeyDownEvent : public Inherit< KeyEvent, KeyDownEvent >
     {
     public:
-        KeyDownEvent( Window* window, Key::Code& key, Key::Code& modified_key, Key::Mod& mod )
-        : Inherit( window, key, modified_key, mod ) 
+        KeyDownEvent( Window* window, Key::ScanCode& code, Key::Code& key, Key::Mod& mod )
+        : Inherit( window, code, key, mod ) 
         {};
     };
 
     class AEON_DLL KeyUpEvent : public Inherit< KeyEvent, KeyUpEvent >
     {
     public:
-        KeyUpEvent( Window* window, Key::Code& key, Key::Code& modified_key, Key::Mod& mod ) 
-        : Inherit( window, key, modified_key, mod ) 
+        KeyUpEvent( Window* window, Key::ScanCode& code, Key::Code& key, Key::Mod& mod )
+        : Inherit( window, code, key, mod ) 
         {};
     };
 
     class AEON_DLL KeyHoldEvent : public Inherit< KeyEvent, KeyHoldEvent >
     {
     public:
-        KeyHoldEvent( Window* window, Key::Code& key, Key::Code& modified_key, Key::Mod& mod, 
+        KeyHoldEvent( Window* window, Key::ScanCode& code, Key::Code& key, Key::Mod& mod,
                       uint32_t repeat_count ) 
-        : Inherit( window, key, modified_key, mod ), m_repeat_count( repeat_count ) 
+        : Inherit( window, code, key, mod ), m_repeat_count( repeat_count ) 
         {};
 
         uint32_t repeats() const { return m_repeat_count; }
