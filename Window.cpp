56c116184d243ca058129a16b43e0709771802cf
Hammy Cooper <thehamstarr@gmail.com>
2023-02-02 00:06:05 +0800

    update() body TODO

--- Window.cpp
+++ Window.cpp
@@ -5,11 +5,12 @@ namespace AEON
     
 void AEON_API Window::Update()
 {
-    while( m_viewport->AdvanceFrame() )
-    {
-        m_viewport->Update();
-        m_viewport->Present();
-    }
+    // TODO implement viewport functions
+    //while( m_viewport->AdvanceFrame() )
+    //{
+    //    m_viewport->Update();
+    //    m_viewport->Present();
+    //}
 }
     
 } // namespace AEON::Graphics
