5f5b04da8604b0f577e3e3e90bc9cf31471aedaa
Hammy Cooper <thehamstarr@gmail.com>
2023-04-13 16:58:26 +0800

    attempt to constrain Dispatch()

--- Event.h
+++ Event.h
@@ -14,12 +14,23 @@ namespace AEON
         bool Dispatch( const T& target, const F& function )
         {
             if( typeid( *this ) != typeid( E ) ) return false;
-            AE_INFO( "Dispatching %s to %s", type_name( static_cast<E&>(*this) ), type_name( function ) );
 
             _handled |= (target->*function)( static_cast<E&>(*this) );
-            AE_WARN_IF( !_handled, "Unhandled %s", type_name( static_cast<E&>(*this) ) );
+            AE_WARN_IF( !_handled, "Unhandled %s", type_name<E>() );
             return _handled;
         }
+
+        //* Attempt #384293 at using concepts to constrain template deduction.
+        //template< typename E, typename F >
+        //requires std::invocable< F, E& >
+        //bool Dispatch( const F& function )
+        //{
+        //    if( typeid( *this ) != typeid( E ) ) return false;
+
+        //    _handled |= function( static_cast<E&>(*this) );
+        //    AE_WARN_IF( !_handled, "Unhandled %s", type_name( E ) );
+        //    return _handled;
+        //}
 
     private:
         bool      _handled = false;
